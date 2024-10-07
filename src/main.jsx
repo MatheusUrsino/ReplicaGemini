aa1806e1166edff7760127899cca067078c4bb83
Matheus Ursino dos Santos <66560955+MatheusUrsino@users.noreply.github.com>
2024-10-07 14:46:39 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ContextProvider from './Context/Context.jsx'
+import ContextProvider from './Context/context.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ContextProvider>
     <App />
